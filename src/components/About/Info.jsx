a03d92b5c75f4a51922dbe31686fa1be69cf2aaf
YHuang <yhuang.huang@mail.utoronto.ca>
2023-07-03 18:07:21 +0800

    added content to about me

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -6,18 +6,16 @@ const Info = () => {
         <div className="about__box">
             <i class="uil uil-archive-alt about__icon"></i>
             <h3 className="about__title">Interest</h3>
-            <span className="about__subtitle">Data Analysis Data Engineering Web Development</span>
-        </div>
-        <div className="about__box">
-            <i class="uil uil-award-alt about__icon"></i>
-            <h3 className="about__title">Membership</h3>
-            <span className="about__subtitle">UofT CSEd</span>
+            <span className="about__subtitle">Data Analysis <br /> Data Engineering <br /> Web Development</span>
         </div>
         <div className="about__box">
             <i class="uil uil-bag about__icon"></i>
-            <h3 className="about__title">Grades</h3>
-            <span className="about__subtitle">3.48/4.0 GPA @ UofT 3.27/4.3 GPA @ HKUST</span>
+            <h3 className="about__title">Academic</h3>
+            <span className="about__subtitle">3.48/4.0 GPA @ UofT 3.27/4.3 GPA @ HKUST Member of UofT CSEd</span>
         </div>
+
+        
+        <img src="https://github-readme-stats.vercel.app/api?username=yhuang1berta&show_icons=true&theme=graywhite&bg_color=FFFFFF&text_bold=false&rank_icon=github&custom_title=My Github Stats (≧▽≦)" alt=''/>
     </div>
   )
 }
